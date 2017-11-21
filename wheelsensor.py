f3ed1be19f182b967406f77318ace8856e20dc53
justindecastri <32371107+justindecastri@users.noreply.github.com>
2017-11-21 18:01:40 -0500

    Add files via upload

--- wheelsensor.py
+++ wheelsensor.py
@@ -1,12 +1,8 @@
-#Create 1 file per wheel and change hallgpio  to represent the physical GPIO connection that you are using.
-
-#### wheel mappings ######
-## right front wheel 26 ##
-## right rear wheel 19 ###
-## left front wheel 20 ###
-## left rear wheel 16  ####
-##########################
-
+# wheel mappings
+# right front wheel 26
+# right rear wheel 19
+# left front wheel 20
+# left rear wheel 16
 
 import RPi.GPIO as GPIO
 import time
@@ -16,53 +12,58 @@ import multiprocessing
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-magnetdetection = 0
-distancetraveled = 0
-revolutions = 0
-wheelspeed = 0
+pulse = 0
+wheeldistance = 0
+wheelrpm = 0.00
+speed = 0.00
 wheel_in = 9.5
-hallgpio = 19
-elapsedtime = 0.00
+hall = 26
+elapse = 0.00
 
 start = time.time()
 
-GPIO.setup(hallgpio, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(hall, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-def get_revolutions():
-    return revolutions
+def get_wheelrpm():
+    return wheelrpm
 
-def get_wheelspeed():
-    return wheelspeed
 
-def get_distancetraveled():
-    return distancetraveled
+def get_speed():
+    return speed
 
-def get_elapsedtime():
-    return elapsedtime
 
-def get_magnetdetection(number):
-    global elapsedtime,distancetraveled,start,magnetdetection,wheelspeed,revolutions,multiplier
-    rotation = 0
-    magnetdetection+=1
-    rotation+=1
-    #revolutions = 0
-    if magnetdetection > 0:
-        elapsedtime = time.time() - start
-        #print ('LeftRearContact')
-        magnetdetection -=1
-    if rotation > 0:
-        distancetraveled += wheel_in
-        #print ('rotation is ' , rotation)
-        rotation -= 1
+def get_wheeldistance():
+    return wheeldistance
 
-    revolutions = 1/elapsedtime *60
+
+def get_elapse():
+    return elapse
+
+
+def get_pulse(number):
+    global elapse, wheeldistance, start, pulse, speed, wheelrpm, multiplier
+    cycle = 0
+    pulse += 1
+    cycle += 1
+    # rpm = 0
+    if pulse > 0:
+        elapse = time.time() - start
+        # print ('LeftRearContact')
+        pulse -= 1
+    if cycle > 0:
+        wheeldistance += wheel_in
+        # print ('cycle is ' , cycle)
+        cycle -= 1
+
+    wheelrpm = 1 / elapse * 60
     start = time.time()
     sleep(.001)
 
 try:
-    GPIO.add_event_detect(hallgpio,GPIO.FALLING,callback = get_magnetdetection,bouncetime=250)
-    #GPIO.cleanup()
-except KeyboardInterrupt:50
-#except elapsedtime < 1:
+
+    GPIO.add_event_detect(hall, GPIO.FALLING, callback=get_pulse, bouncetime=20)
+    # GPIO.cleanup()
+except KeyboardInterrupt:
+    # except elapse < 1:
     GPIO.cleanup()
